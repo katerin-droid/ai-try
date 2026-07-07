9f742743e056bd6303aeb4947755273a9cb4fa84
katerin-droid <2960219600@qq.com>
2026-07-07 18:26:35 +0800

    修改A

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
->     public static void main(String[] args) {
->         System.out.println("初始版本：Hello Git");
->     }
-> }
+    public static void main(String[] args) {
+        System.out.println(修改);
+    }
+}
